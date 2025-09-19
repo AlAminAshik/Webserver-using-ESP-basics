3436ac62075ac4efbebe04d0312963847830f3d6
AlAminAshik <alaminashik890.bd@gmail.com>
2025-09-20 01:16:01 +0600

    control led via websocket

--- Web browser using esp/src/local_web_server.cpp
+++ Web browser using esp/src/local_web_server.cpp
@@ -19,6 +19,8 @@ const int sensorPin = 36; // Analog pin for potentiometer
 int rawData = 0;
 static unsigned long previousMillis = 0;
 
+const int ledPin = 2; // GPIO2 is the built-in LED pin
+
 // // HTML page that refreshes itself every second to show the potentiometer value
 // String htmlPage() {
 //   String page = "<!DOCTYPE html><html><head>";
@@ -30,12 +32,36 @@ static unsigned long previousMillis = 0;
 //   return page;
 // }
 
-// HTML page with WebSocket client
+// // HTML page with WebSocket client (only show potentiometer value)
+// const char index_html[] PROGMEM = R"rawliteral(
+// <!DOCTYPE html>
+// <html>
+// <head>
+//   <title>ESP32 Potentiometer Values</title>
+//   <script>
+//     var socket;
+//     function init() {
+//       socket = new WebSocket("ws://" + window.location.host + "/ws");
+//       socket.onmessage = function(event) {
+//         document.getElementById("pot").innerHTML = event.data + " ohms";
+//       };
+//     }
+//     window.onload = init;
+//   </script>
+// </head>
+// <body>
+//   <h2>ESP32 Live Potentiometer (WebSocket)</h2>
+//   <p>Potentiometer: <span id="pot">--</span></p>
+// </body>
+// </html>
+// )rawliteral";
+
+// HTML + JS (to control led and view pot value from web browser)
 const char index_html[] PROGMEM = R"rawliteral(
 <!DOCTYPE html>
 <html>
 <head>
-  <title>ESP32 Potentiometer Values</title>
+  <title>ESP32 Live Potentiometer (WebSocket)</title>
   <script>
     var socket;
     function init() {
@@ -44,34 +70,61 @@ const char index_html[] PROGMEM = R"rawliteral(
         document.getElementById("pot").innerHTML = event.data + " ohms";
       };
     }
+    function ledOn() {
+      socket.send("LED_ON");
+    }
+    function ledOff() {
+      socket.send("LED_OFF");
+    }
     window.onload = init;
   </script>
 </head>
 <body>
   <h2>ESP32 Live Potentiometer (WebSocket)</h2>
+  <hr>
   <p>Potentiometer: <span id="pot">--</span></p>
+  <button onclick="ledOn()">Turn LED ON</button>
+  <button onclick="ledOff()">Turn LED OFF</button>
 </body>
 </html>
 )rawliteral";
 
 
-//Serial print is a client (web browser is connected)
+//on website event
 void onWsEvent(AsyncWebSocket * server, AsyncWebSocketClient * client,
                AwsEventType type, void * arg, uint8_t *data, size_t len) {
   if (type == WS_EVT_CONNECT) {
-    Serial.printf("Client %u connected\n", client->id());
-  } else if (type == WS_EVT_DISCONNECT) {
-    Serial.printf("Client %u disconnected\n", client->id());
+    Serial.printf("Client %u connected\n", client->id());   //serial print if a client (web browser) is connected
+  } 
+  else if (type == WS_EVT_DISCONNECT) {
+    Serial.printf("Client %u disconnected\n", client->id());    //serial print if a client (web browser) is disconnected
+  } 
+  else if (type == WS_EVT_DATA) {     //when data is received from client
+    //data is not null terminated
+    String msg = "";
+    for (size_t i = 0; i < len; i++) {
+      msg += (char)data[i];
+    }
+    Serial.println("Received: " + msg); //print received message
+
+    if (msg == "LED_ON") {
+      digitalWrite(ledPin, HIGH);       //turn on board led on
+    } else if (msg == "LED_OFF") {
+      digitalWrite(ledPin, LOW);        //turn off board led off
+    }
   }
+
 }
-unsigned long lastSend = 0;
+
+unsigned long lastSend = 0;     //keep track of data being send to browser
 
 
 void setup() {
   Serial.begin(115200);
   delay(1000);
+  pinMode(ledPin, OUTPUT); // Initialize the built-in LED pin as an output
 
-  WiFi.softAP(apSSID, apPassword);
+  WiFi.softAP(apSSID, apPassword);  // Start the access point
 
   Serial.println("Access Point started");
   Serial.print("SSID: ");
@@ -88,8 +141,8 @@ void setup() {
   server.addHandler(&ws);
 
   // Serve HTML
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send_P(200, "text/html", index_html);
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){  //when root is requested
+    request->send_P(200, "text/html", index_html);  //send webpage
   });
 
   // Start server
@@ -98,6 +151,8 @@ void setup() {
 }
 
 void loop() {
+  ws.cleanupClients(); //clean up old clients by handling disconnection safely.
+
   rawData = analogRead(sensorPin);
   unsigned long currentMillis = millis();
   //read and print the sensor value every second
@@ -107,8 +162,9 @@ void loop() {
     }
 
     unsigned long now = millis();
-    if (now - lastSend > 1000) {  // send every 1s
-        ws.textAll(String(rawData, 10)); //notify all clients (decimal (base 10) format)
+    if (now - lastSend > 500) {  // send every 500ms (sending data very fast can cause connection to close)
+        //check if clients are connected before sending to avoid reset
+        if(ws.count() > 0) ws.textAll(String(rawData, 10)); //notify all clients (decimal (base 10) format)
         lastSend = now;
     }
 }
